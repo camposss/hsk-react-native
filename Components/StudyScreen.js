06282243a367d956c2c0edbb0faa9e2dc98a53ec
camposss <ccamposs23@gmail.com>
2019-03-02 21:20:50 -0800

    Basic card styling with some chinese data inputted.

--- Components/StudyScreen.js
+++ Components/StudyScreen.js
@@ -1,31 +1,107 @@
-import React, {Component} from "react";
-import { View, Text, Button } from "react-native";
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Card, Icon, Image, Button } from "react-native-elements";
+import hsk1 from "../assets/hsk-vocab/hsk1.json";
+
 import InfoModal from "./Modals/InfoModal";
 
 class StudyScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
     const params = navigation.state.params || {};
     return {
-      headerTitle: 'Study',
+      headerTitle: "Flashcard",
       headerRight: (
         <Button
-          onPress={() => navigation.navigate('Info')}
+          onPress={() => navigation.navigate("Info")}
           title="Info"
-          color="blue"
-      />
-      ),
+          type = "clear"
+        />
+      )
     };
   };
+  constructor(props){
+    super(props)
+    this.state = {
+      currentHskData: []
+    }
+  }
+
+  renderHskData(){
+    //determine level-- for now will be hsk1
+
+    //return random index of array
+
+    let randomIndex = Math.floor(Math.random()* hsk1.length + 1);
+    console.log(hsk1[randomIndex])
+    if(hsk1[randomIndex]) {
+      return hsk1[randomIndex];
+    }
+  }
 
   render() {
-
+    const currentHskWordData = this.renderHskData()
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Study Screen</Text>
+      <View style={styles.container}>
+        <Card
+          containerStyle={styles.cardContainer}
+          title="Shanghai, China"
+          image={require("../assets/images/shanghai.png")}
+        >
+          <View style = {styles.bottomCardSectionContainer}>
+            <View style= {styles.studyTextContainer}>
+              <Text style={styles.chinese_character }>{currentHskWordData.chinese_character}</Text>
+              <Text style={styles.example_sentence }>{currentHskWordData.pinyin}</Text>
+              <Text style={styles.example_chinese_sentence}>{currentHskWordData.chinese_sentence_1}</Text>
+              <Text style={styles.example_english_sentence}>{currentHskWordData.english_sentence_1}</Text>
+            </View>
+            <Button
+              title="Flip Card"
+              style = {styles.flip_card_button}
+              // onPress={() => this.props.navigation.navigate("Details")}
+            />
+          </View>
+
+        </Card>
       </View>
     );
   }
 }
 
-
 export default StudyScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignContent: "center", 
+    // justifyContent: "center"
+  },
+  cardContainer: {
+    // flex: 1,
+    height: "95%",
+    backgroundColor: "white"
+  },
+  bottomCardSectionContainer: {
+    justifyContent: "space-between"
+  },
+  studyTextContainer: {
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  chinese_character: {
+    fontSize: 32,
+    marginTop: "2%",
+    marginBottom: '5%'
+  },
+  example_chinese_sentence: {
+    fontSize: 20,
+    marginBottom: '5%'
+  },
+  example_english_sentence: {
+    fontSize: 14,
+    marginBottom: '5%'
+  },
+  flip_card_button: {
+    marginTop: "50%"
+  }
+
+});
